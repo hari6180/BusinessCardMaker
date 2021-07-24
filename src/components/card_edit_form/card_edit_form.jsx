c2184e7bdd0f5f79c883a92f720c96a055cf0aa3
Hari Jeon <ori080525@gmail.com>
2021-07-24 15:45:47 +0900

    add card maker html

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styles from "./card_edit_form.module.css";
 
-const CardEditForm = ({ card }) => <h1>{card.id}</h1>;
+const CardEditForm = ({ card }) => {
+  const { name, company, title, email, message, theme, fileName, fileURL } = card;
+  return (
+    <form>
+      <input type="text" name="name" value={name} />
+      <input type="text" name="company" value={company} />
+      <select name="theme" value={theme}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">colorful</option>
+      </select>
+      <input type="text" name="title" value={title} />
+      <input type="text" name="email" value={email} />
+      <textarea name="message" value={message} />
+    </form>
+  );
+};
 
 export default CardEditForm;
