2916d5bbe57f5dd74394f0daf975bccd1b07245f
Hari Jeon <ori080525@gmail.com>
2021-07-24 19:51:42 +0900

    add CardAddForm component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -57,11 +57,16 @@ const Main = ({ authService }) => {
     });
   });
 
+  const addCard = (card) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  };
+
   return (
     <section className={styles.main}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} />
+        <Editor cards={cards} addCard={addCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
